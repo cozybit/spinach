c3628612e07a50782863c58ab75ac1823629c20c
Thomas Pedersen <thomas@cozybit.com>
2012-06-28 16:31:36 -0700

    spinach: add -a flag to release.sh
    create a release for all target types with
    
    $ ./scripts/release.sh -a -v<version>
    
    Also include a RELEASE_NOTES_TEMPLATE and open it for editing if
    building a full release. Also Put a "plant_mesh_<version>.tgz" in the
    spinach root.

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,24 +4,39 @@
 
 source `dirname $0`/common.sh
 
-USAGE="usage: $0 -t <type> -v <version>"
-while getopts  "t:v:" options; do
+USAGE="usage: $0 [-t <type> | -a] -v <version>"
+while getopts  "t:v:a" options; do
         case $options in
 		t ) TYPE=$OPTARG;;
 		v ) V=$OPTARG;;
+		a ) ALL=yes;;
 		* ) fail $USAGE
         esac
 done
 
-[ -z "$TYPE" ] && fail "please specify a node type!"
+[ -z "$TYPE" -a -z "$ALL" ] && fail "please specify a node type!"
 [ -z "$V" ] && fail "please specify a release version!"
 
+TARGETS=$TYPE
+[ "$ALL" == "yes" ] && TARGETS="$SPINACH_TYPES"
+
 ./scripts/fetch.sh
-./scripts/configure.sh -t $TYPE
-./scripts/build.sh || fail
+for target in $TARGETS; do
+	./scripts/configure.sh -t $target
+	echo "building release for $target"
+	./scripts/build.sh >/dev/null || fail "$target build failed"
+	mkdir -p releases/$V/$target/
+	cp `get_openwrt_img $target factory` releases/$V/$target/plant_mesh-$target-factory.bin || fail
+	cp `get_openwrt_img $target sysupgrade` releases/$V/$target/plant_mesh-$target-sysupgrade.bin || fail
+done
 
-mkdir -p releases/$V/$TYPE/
-cp `get_openwrt_img $TYPE factory` releases/$V/$TYPE/plant_mesh-$TYPE-factory.bin
-cp `get_openwrt_img $TYPE sysupgrade` releases/$V/$TYPE/plant_mesh-$TYPE-sysupgrade.bin
+# ok so we're not testing..
+if $ALL == "yes"; then
+	cp releases/RELEASE_NOTE_TEMPLATE releases/$V/RELEASE_NOTES
+	vi releases/$V/RELEASE_NOTES
+	cd releases/
+	tar cvzf ../plant_mesh_$V.tgz $V/
+	cd ../
+fi
 
-echo "release $V for $TYPE done"
+echo "release $V for $TARGETS done"
