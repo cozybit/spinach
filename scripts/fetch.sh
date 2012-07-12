f88c4fca5e2703f92c9b56b3da99fde262e72fb4
Jason Abele <jason@cozybit.com>
2012-07-11 17:04:02 -0700

    Set revisions for openwrt feeds

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -5,10 +5,13 @@ source `dirname $0`/common.sh
 git clone git://nbd.name/openwrt.git --no-checkout
 pushd openwrt
 git checkout ${OPENWRT_COMMIT}
-./scripts/feeds update
-./scripts/feeds install -a
 
 # apply patches
 git am ../patches/*
+
+# update the feeds
+./scripts/feeds update
+./scripts/feeds install -a
+
 popd > /dev/null
 
