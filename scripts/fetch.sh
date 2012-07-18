ec12aac8e754f9cc84cfc8e986445bd7aeffe3e7
Guillermo A. Nunez <guillermo@cozybit.com>
2012-07-17 20:00:22 -0700

    Delete redundant fucntions: die.

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -2,13 +2,13 @@
 
 source `dirname $0`/common.sh
 
-[ -d openwrt ] && die "ERROR: Default openwrt directory already exits. Aborting." 
-git clone git://nbd.name/openwrt.git --no-checkout || die "ERROR: git clone failed. Aborting. "
+[ -d openwrt ] && fail "ERROR: Default openwrt directory already exits. Aborting." 
+git clone git://nbd.name/openwrt.git --no-checkout || fail "ERROR: git clone failed. Aborting. "
 pushd openwrt
-git checkout ${OPENWRT_COMMIT} || die "ERROR: git checkout failed. Aborting."
+git checkout ${OPENWRT_COMMIT} || fail "ERROR: git checkout failed. Aborting."
 
 # apply patches
-git am ../patches/* || die "ERROR: patches werent able to apply cleanly. Aborting."
+git am ../patches/* || fail "ERROR: patches werent able to apply cleanly. Aborting."
 
 # Clean out the feeds so we get the right rev on feeds update
 ./scripts/feeds clean
